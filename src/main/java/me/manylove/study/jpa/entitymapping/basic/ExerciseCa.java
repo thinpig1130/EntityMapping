8435d79a00e034e7e62e7ef5a62676f7d7e65a5e
권다애 <kwondarc@plani.co.kr>
2022-12-17 23:38:04 +0900

    예제 코드 정리 시작

--- src/main/java/me/manylove/study/jpa/entitymapping/basic/ExerciseCa.java
+++ src/main/java/me/manylove/study/jpa/entitymapping/basic/ExerciseCa.java
@@ -14,25 +14,39 @@ import java.util.Date;
 import java.util.UUID;
 
 /**
- * == C. 컬럼 매핑 =====================================================
+ * == C. 필드와 컬럼 매핑 =====================================================
  * 자바속성과 데이터베이스의 컬럼은 어떻게 매핑 할 수 있을까?
  * 컬럼매핑을 위해 사용되는 어노테이션의 종류와 속성들에 대해서 알아봅니다.
  *
  *  = @Column
  *    컬럼을 매핑한다.
- *    - name : 매핑할 컬럼 이름
- *    - insertable : false 생성 후 저장할 때는 해당 속성은 넣지 않는다.
- *    - updatable : false 생성 후에 수정할 때는 해당 필드를 수정하지 않는다.
- *    - table : 하나의 엔티티를 두 개 이상의 테이블에 매핑 할 때 사용한다.
- *    - unique, nullable, insertable, updatable, columnDefinition, length, precision, scale -> DDL 관련 속성
+ *    - name                : 매핑할 컬럼 이름
+ *    - unique              : [DDL] unique 제약조건이 추가 여부
+ *                            true / false (기본값)
+ *    - nullable            : [DDL] NOT NULL 제약조건 추가 여부
+ *                            true (기본값) / false
+ *    - insertable          : 생성시 속성 입력값 데이터 베이스 저장 여부
+ *                            true (기본값) / false
+ *    - updatable           : 업데이트 시 변경 값 데이터 베이스 저장 여부
+ *                            true (기본값) / false
+ *    - columnDefinition    : [DDL] 데이터베이스 컬럼 정보를 직접 입력
+ *    - table               : 하나의 엔티티를 두 개 이상의 테이블에 매핑 할 때 사용
+ *    - length              : [DDL] 문자길이 제약 조건.
+ *                            String 타입에만 사용.
+ *    - precision           : [DDL] 소수점을 포함한 전체 자리 수
+ *                            BigDecimal , BigInteger 타입에서만 사용
+ *    - scale               : [DDL] 소수의 자리수
+ *                            BigDecimal , BigInteger 타입에서만 사용
  *
  *  - @Enumerated
  *    enum 타입 컬럼
- *    - value : 타입을 데이터 베이스에 매핑하는 방법
+ *    - value               : 저장 방법을 설정합니다.
+ *                            EnumType.ORDINAL (기본값) / EnumType.STRING
  *
  *  - @Temporal
  *    자바의 Date 타입과 Calendar 타입을 데이터베이스에서 사용하는 특정 타입과 매핑한다.
- *    - value : 매핑할 타입
+ *    - value               : 매핑할 타입
+ *                            TemporalType.DATE / TemporalType.TIME / TemporalType.TIMESTAMP (기본값)
  *
  *  - @Lob
  *    BLOB, CLOB 타입을 매핑한다.
@@ -55,7 +69,7 @@ public class ExerciseCa {
     private UUID id;
 
     /** 문자열 타입 컬럼 매핑 **/
-    private String P01_StringDefault;
+    private String P01_string;
 
     @Lob
     private String P02_stringLob;
@@ -63,83 +77,189 @@ public class ExerciseCa {
     @Column(length = 500)
     private String P03_stringLength500;
 
-    @Column(length = 20)
-    private int P03_intLength20;
+    /**
+     * [ 코드를 통해 살펴 볼 내용 ]
+     * 1. @Column 의 length 속성은 문자열 타입에만 사용되는 DDL 속성이다.
+     *    다른 타입에는 길이를 지정해도 별 영향을 미치지 않는다.
+     * 2. @Lob 은 BLOB, CLOB 과 같은 대형 크기의 DATABASE 타입을 매핑한다.
+     */
 
     /** 정수 타입 매핑 **/
 
-    private int P04_intDefault;
+    private int P10_int;
 
-    private long P0_longDefault;
+    private Integer P11_Integer;
 
-    private BigInteger P0_bigInteger;
+    private long P12_longPrimitive;
+
+    private Long P13_LongReference;
+
+    private BigInteger P14_bigInteger;
 
     @Column(precision = 20)
-    private BigInteger P0_bigIntegerPrecision;
+    private BigInteger P15_bigIntegerPrecision;
+
+    /**
+     * [ 코드를 통해 살펴 볼 내용 ]
+     * 3. 자바객체 타입의 길이에 맞게 데이터 베이스 유형이 결정되어 생성된다.
+     * 4. BigInteger 타입의 경우, precision 값으로 생성되는 숫자의 길이를 결정할 수 있다.
+     */
 
     /** 실수 타입 매핑 **/
 
-    private float P0_floatDefault;
+    private float P20_float;
 
-    private double P0_doubleDefault;
+    private double P21_double;
 
-    private BigDecimal P0_bigDecimal;
+    private BigDecimal P23_bigDecimal;
 
     @Column(precision = 10, scale = 2)
-    private BigDecimal P0_bigDecimalPrecisionScale;
+    private BigDecimal P24_bigDecimalPrecisionScale;
+
+    /**
+     * [ 코드를 통해 살펴 볼 내용 ]
+     * 5. 자바객체 타입의 길이에 맞게 데이터 베이스 유형이 결정되어 생성된다.
+     * 6. BigDecimal 타입의 경우, precision 값과 scale 값을 지정하여 생성되는 타입의 길이를 결정할 수 있다.
+     */
 
     /** enum type 컬럼 매핑 **/
 
-    private ExType P0_enumDefault;
+    private ExType P30_enum;
 
     @Enumerated
-    private ExType P0_enumerated;
+    private ExType P31_enumerated;
 
     @Enumerated(EnumType.ORDINAL)
-    private ExType P0_enumeratedTypeOrdinal;
+    private ExType P32_enumeratedTypeOrdinal;
 
     @Enumerated(EnumType.STRING)
-    private ExType P0_enumeratedTypeString;
+    private ExType P33_enumeratedTypeString;
+
+    /**
+     * [ 코드를 통해 살펴 볼 내용 ]
+     * 7. 위의 3개는 같은 결과를 같는다.
+     * 8. EnumType.STRING 은 타입명을 문자로 저장할때 사용한다.
+     * 9. EnumType.ORDINAL의 경우, enum의 순서가 변경되면 기존 데이터에 영향을 주게되는 단점이 있다.
+     */
 
     /** 날짜/시간 관련 컬럼 매핑 **/
 
-    private LocalDate P0_localDate;
+    private LocalDate P40_localDate;
 
-    private LocalDateTime P0_localDateTime;
+    private LocalDateTime P41_localDateTime;
 
-    private Date P0_date;
-
-    @Temporal(TemporalType.DATE)
-    private Date P0_dateTemporalTypeDate;
-
-    @Temporal(TemporalType.TIME)
-    private Date P0_dateTemporalTypeTime;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date P0_dateTemporalTypeTimestamp;
-
-    private Calendar P0_calendar;
+    private Date P42_date;
 
     @Temporal(TemporalType.DATE)
-    private Calendar P0_calendarTemporalTypeDate;
+    private Date P44_dateTemporalTypeDate;
 
     @Temporal(TemporalType.TIME)
-    private Calendar P0_calendarTemporalTypeTime;
+    private Date P45_dateTemporalTypeTime;
 
     @Temporal(TemporalType.TIMESTAMP)
-    private Calendar P0_calendarTemporalTypeTimestamp;
+    private Date P46_dateTemporalTypeTimestamp;
+
+    private Calendar P47_calendar;
+
+    @Temporal(TemporalType.DATE)
+    private Calendar P48_calendarTemporalTypeDate;
+
+    @Temporal(TemporalType.TIME)
+    private Calendar P49_calendarTemporalTypeTime;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar P50_calendarTemporalTypeTimestamp;
+
+    /**
+     * [ 코드를 통해 살펴 볼 내용 ]
+     * 10. @Temporal 년,월,일,시,분,초 가 있는 자바의 Date 타입과
+     *     날짜, 시간, 날짜와 시간이라는 3가지 데이터 타입이 존재하는 DATABASE의 타입을 매핑하기 위해 사용된다.
+     */
+
+    /** 불리언 타입 매핑 **/
+    private boolean P60_booleanPrimitive;
+
+    private Boolean P60_booleanReference;
+
+    /**
+     * [ 코드를 통해 살펴 볼 내용 ]
+     * 11. 불리언 타입의 경우, 데이터베이스에 따라 저장하는 유형이 제각각이다.
+     */
 
     /** UUID 타입 매핑 **/
+    private UUID P65_UuidDefault;
 
-    private UUID P0_UuidDefault;
+    /**
+     * [ 코드를 통해 살펴 볼 내용 ]
+     * 12. UUID 의 경우, 데이터베이스에 따라 저장하는 유형이 제각각이다.
+     */
 
     /** 매핑 제외 **/
-
     @Transient
-    private String P0_transientProperty;
-}
+    private String P70_transientProperty;
 
-/**
- * [ 코드를 통해 살펴 볼 내용 ]
- * 1. 변경된 엔티티명이 테이블 매핑에 영향을 주어서, 매핑된 테이블 이름이 달라진 것을 볼 수 있다.
- */
+    /**
+     * [ 코드를 통해 살펴 볼 내용 ]
+     * 12. 해당 속성과 매칭되는 필드는 데이터베이스에 존재하지 않는다.
+     */
+
+
+    /** Primitive Type VS Reference Type **/
+
+    private int P71_primitive;
+
+    private Integer P72_reference;
+
+    @Column
+    private int P73_primitiveColumn;
+
+    @Column
+    private Integer P74_referenceColumn;
+
+    @Column(nullable = false)
+    private int P75_primitiveNotNull;
+
+    @Column(nullable = false)
+    private Integer P74_referenceNotNull;
+
+    /**
+     * [ 코드를 통해 살펴 볼 내용 ]
+     * 13. Primitive type의 경우 기본적으로 Not Null 제약조건이 추가된다.
+     * 14. 하지만, @Column의 nullable 타입의 기본속성이 true 이므로 @Column을 붙이면 Not Null 제약조건이 사라진다.
+     * 15. nullable 속성의 값을 넣어주면, Primitive Type 인지 Reference Type 인지와 무관하게 값에 따른 제약조건을 적용된다.
+     */
+
+    /** 그 밖의 속성 살펴보기 **/
+
+    @Column(name="P80_CHANGE_COLUMN_NAME")
+    private String changeColumnName;
+
+    @Column(nullable = false)
+    private String P81_addNotNull;
+
+    @Column(unique = true)
+    private String P82_addUnique;
+
+    @Column(columnDefinition = "NUMERIC(20) UNIQUE")
+    private int P83_addColumnDefinition;
+
+    @Column(insertable = false)
+    private String P84_insertableFalse;
+
+    @Column(updatable = false)
+    private String P85_updatableFalse;
+
+    /**
+     * [ 코드를 통해 살펴 볼 내용 ]
+     * 16. 객체와 필드명과 관계없이 필드명을 직접 입력하여 매핑 할 수도 있다.
+     * 17. Not Null 제약조건을 설정할 수 있다.
+     * 18. Unique 제약조건을 설정할 수 있다.
+     * 19. 컬럼의 정보를 직접 작성할 수 있다.
+     * 20. insertable 과 updatable 속성은 엔티티 매핑에 관련된 속성이 아니다.
+     *    데이터를 입력하고, 수정하는 과정에 영향을 주는 속성이다.
+     *    주로, 필드가 데이터베이스 자체에서 자동으로 채워지는 경우 insertable 을 사용하여 읽기전용 속성으로 만든다.
+     *    또 처음 값을 입력한 다음에, 수정되지 않는 필드 항목에 updatable = false 를 주기도 한다.
+     *    JPA에서 자동으로 채워주는 상속 매핑에 사용되는 값을 읽기전용으로 불러오는 경우에 insertable과 updatable을 false로 변경하지 않으면
+     *    중복으로 해당 값이 채워져 입력 오류가 발생하기도 한다.
+     */
+}
